c9c1c79bf6d379bd67f16ae6389190425c70bcfe
swog <sanicsupersonic@gmail.com>
2023-08-24 23:11:02 -0500

    Interrupt descriptor table working

--- src/boot/start64.asm
+++ src/boot/start64.asm
@@ -1,61 +1,45 @@
 bits 64
 
+extern kmain
+extern kidtstubs
+extern kpremain
+global long_mode_start
+
 section .data
 idt:
-	times 50 dq 0
+	times 256 dq 0
 idtr:
 	dw $ - idt - 1
 	dq idt
 
 section .text
-extern kernel_main
-extern kernel_int_stub
-extern kernel_int_test
-global int_test
-global int_stub
-global idt_load
-global idt_call
-global long_mode_start
-
 	; ===========================================
-	; Default
-int_stub:
-	iretq
-
-int_test:
-	;cli
-	mov ax, 0x10
-	mov gs, ax
-	mov dword [gs:0xb8000], ') : '
-	;hlt
-	iret
-
-idt_load:
-	lidt [rdi]
-	ret
-
-idt_call:
-	int 49
-	ret
+	; RBX-IDT destination
+	; RAX-Address
+	; CX-Increment
 
 idt_init:
 	mov rbx, idt
+	mov rcx, kidtstubs
 .loop:
-	mov rax, int_test
-	mov [rbx], ax
+	mov rax, qword [rcx]
+	
+	mov word [rbx], ax
 	mov word [rbx+2], 0x8
 	mov word [rbx+4], 0x8e00
 	shr rax, 16
-	mov [rbx+6], ax
+	mov word [rbx+6], ax
 	shr rax, 16
-	mov [rbx+8], rax
+	mov qword [rbx+8], rax
+	
 	add rbx, 16
+	add rcx, 16
+	
 	cmp rbx, idtr
 	jne .loop
 
 	lidt [idtr]
 	;sti
-	int 49
 	ret
 
 	; ===========================================
@@ -68,8 +52,10 @@ long_mode_start:
 	mov gs, ax
 	mov ds, ax
 
+	call kpremain
 	call idt_init
+	call kmain
 
-	;call kernel_main
+	int 49
 
 	jmp $
--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,30 @@
 #include "vga.h"
 #include "ktypes.h"
 
-void kernel_main() {
-	//kernel_idt_load();
+void* kidtstubs[256];
+
+void kidtstub() {
+	vga_puts("Hello, world");
+	asm("iret");
+}
+
+void kidtfixstub() {
+	asm("add 8, %rsp");
+	asm("iret");
+}
+
+void kpremain() {
+	for (int i = 0; i < 256; i++) {
+		kidtstubs[i] = kidtstub;
+	}
+	kidtstubs[0x8] = kidtfixstub;
+	kidtstubs[0xA] = kidtfixstub;
+	kidtstubs[0xB] = kidtfixstub;
+	kidtstubs[0xC] = kidtfixstub;
+	kidtstubs[0xD] = kidtfixstub;
+	kidtstubs[0xE] = kidtfixstub;
+}
+
+void kmain() {
+	
 }
