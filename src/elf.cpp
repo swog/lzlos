fca636266bf15321f42678d95f5f07d080bf8c37
swog <swog@github.com>
2024-01-21 00:41:20 -0600

    Elf header library - This library will eventually allow us call libc initialization functions - After that, executables... :')

--- src/elf.cpp
+++ src/elf.cpp
@@ -47,6 +47,7 @@ void* elf_entry(size_t size, const void* base) {
 	return (void*)((Elf64_Ehdr*)base)->e_entry;
 }
 
+// Program header
 Elf64_Phdr* elf_phdr(size_t size, const void* base) {
 	if (size < sizeof(Elf64_Ehdr)) {
 		return NULL;
@@ -61,7 +62,8 @@ Elf64_Phdr* elf_phdr(size_t size, const void* base) {
 	return (Elf64_Phdr*)(((char*)base)+phoff);
 }
 
-Elf64_Shdr* elf_shdr(size_t size, const void* base) {
+// Section header by index
+Elf64_Shdr* elf_shdr(size_t size, const void* base, size_t index) {
 	if (size < sizeof(Elf64_Ehdr)) {
 		return NULL;
 	}
@@ -72,16 +74,57 @@ Elf64_Shdr* elf_shdr(size_t size, const void* base) {
 		return NULL;
 	}
 
-	return (Elf64_Shdr*)(((char*)base)+shoff);	
+	size_t tableoff = ((Elf64_Ehdr*)base)->e_shentsize*index;
+
+	if (size < shoff+tableoff) {
+		return NULL;
+	}
+
+	return (Elf64_Shdr*)(((char*)base)+shoff+tableoff);	
 }
 
+// Section index of the string table.
+size_t elf_shdr_stringtable(size_t size, const void* base) {
+	if (size < sizeof(Elf64_Ehdr)) {
+		return -1;
+	}
 
+	return ((Elf64_Ehdr*)base)->e_shstrndx;
+}
 
+// Section header count
+size_t elf_shdr_count(size_t size, const void* base) {
+	if (size < sizeof(Elf64_Ehdr)) {
+		return -1;
+	}
 
+	return ((Elf64_Ehdr*)base)->e_shnum;
+}
 
+// Get section name table.
+// Elf64_Shdr's sh_name is an offset into this table.
+const char* elf_shdr_names(size_t size, const void* base) {
+	size_t strndx = elf_shdr_stringtable(size, base);
+	Elf64_Shdr* strshdr = elf_shdr(size, base, strndx);
 
+	if (!strshdr) {
+		return NULL;
+	}
 
+	if (size < strshdr->sh_offset) {
+		return NULL;
+	}
 
+	return (const char*)base+strshdr->sh_offset;
+}
 
+// Get section header name by section index
+const char* elf_shdr_name(size_t size, const void* base, size_t index) {
+	const char* names = elf_shdr_names(size, base);
+	
+	if (!names) {
+		return NULL;
+	}
 
-
+	return names+elf_shdr(size, base, index)->sh_name;
+}
--- src/libelf.h
+++ src/libelf.h
@@ -24,9 +24,20 @@ void* elf_entry(size_t size, const void* base);
 
 // Program header table
 Elf64_Phdr* elf_phdr(size_t size, const void* base);
-// Section header table
-Elf64_Shdr* elf_shdr(size_t size, const void* base);
+// Section header by index
+Elf64_Shdr* elf_shdr(size_t size, const void* base, size_t index);
 
+// Section index of the string table, into the section table
+size_t elf_shdr_stringtable(size_t size, const void* base);
 
+// Section header count
+size_t elf_shdr_count(size_t size, const void* base);
+
+// Get section name table.
+// Elf64_Shdr's sh_name is an offset into this table.
+const char* elf_shdr_names(size_t size, const void* base);
+
+// Get section header name by section index
+const char* elf_shdr_name(size_t size, const void* base, size_t index);
 
 #endif
